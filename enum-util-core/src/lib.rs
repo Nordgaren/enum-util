cb013b9c2d3948084e0a7f4be936935ce7dbf6aa
Nordgaren <nordgarentv@gmail.com>
2024-05-18 13:03:10 -0700

    fix up generic and const support

--- enum-util-core/src/lib.rs
+++ enum-util-core/src/lib.rs
@@ -25,15 +25,32 @@ fn variant_values_internal(_args: TokenStream, item: TokenStream) -> Result<Toke
     let name = &enum_item.ident;
     let generics = &enum_item.generics;
     let mut generics_cleaned = enum_item.generics.clone();
+    let mut cleaned_params = Punctuated::new();
     for param in generics_cleaned.params.iter_mut() {
-        match param {
-            GenericParam::Type(t) => t.bounds = Punctuated::new(),
-            GenericParam::Lifetime(_) => {}
-            GenericParam::Const(_) => {}
-        }
+        let new_param = match param {
+            GenericParam::Type(t) => {
+                t.bounds = Punctuated::new();
+                GenericParam::Type(t.clone())
+            },
+            GenericParam::Const(c) => GenericParam::Type(TypeParam {
+                attrs: vec![],
+                ident: c.ident.clone(),
+                colon_token: None,
+                bounds: Default::default(),
+                eq_token: None,
+                default: None,
+            }),
+            GenericParam::Lifetime(l) => GenericParam::Lifetime(l.clone()),
+            _ => continue,
+        };
+
+        cleaned_params.push_value(new_param);
+        cleaned_params.push_punct(Comma::default())
     }
+
+    generics_cleaned.params = cleaned_params;
+
     let where_clause = &generics.where_clause;
-    eprintln!("{:?}", where_clause);
 
     let impls = quote! {
         impl #generics #name #generics_cleaned #where_clause {
